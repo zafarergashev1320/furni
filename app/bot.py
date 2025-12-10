d3fe824aa7d43b91db4a8617f67b5ff199abd80a
zafarergashev1320 <zafarergashev435@gmail.com>
2025-12-10 08:47:35 +0500

    add

--- app/bot.py
+++ app/bot.py
@@ -1,27 +1,11 @@
-from aiogram import types, Bot, Dispatcher, F
-import asyncio
-from aiogram.filters import Command
+from aiogram import Bot, Dispatcher
 from environs import Env
 
 env = Env()
 env.read_env()
 
 API_TOKEN = env.str("API_TOKEN")
-CHAT_ID = env.str("CHAT_ID")
-WEBHOOK_URL = env.str("WEBHOOK_URL")
+CHAT_ID = int(env.str("CHAT_ID"))  # ✅ integerga o‘tkazish
+
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
-
-
-
-@dp.message(Command("start"))
-async def send_message(message: types.Message):
-    await message.answer(text="Sa")
-
-
-async def setup_webhook():
-    await bot.set_webhook(
-        url=WEBHOOK_URL,
-        allowed_updates=["message", "callback_query"],
-        drop_pending_updates=True)
-
